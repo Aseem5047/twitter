057c3103a018f5a252ce2115edb0c01dd066c0c8
Aseem5047 <aseemgupta2002@gmail.com>
2023-08-03 16:18:55 +0530

    latest updates

--- pages/chats.tsx
+++ pages/chats.tsx
@@ -4,12 +4,10 @@ import React from "react";
 const Chats = () => {
 	return (
 		<div className="flex justify-center align-center w-full h-full">
-			<Image
+			<img
 				src="https://miro.medium.com/v2/resize:fit:1358/1*zBFBJktPD3_z0S_35kO5Hg.gif"
 				alt=""
-				height={75}
-				width={75}
-				className="flex-grow rounded-lg h-[85%]"
+				className="flex-grow rounded-lg h-[85%] mt-4"
 			/>
 		</div>
 	);
